ed96e40a17ce106526e2f01ec10408a807da03a2
Sebastian Too <s.too@reply.it>
2021-03-19 16:12:24 +0800

    4e0b3e0

---
+++ _nuxt/static/1616141435/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fwork","\u002Fcontact","\u002Fresume","\u002Fwork\u002Fsolar-turbine","\u002Fwork\u002Fpoor-economics-of-desa-mentari"]})
---
+++ _nuxt/static/1616141435/work/poor-economics-of-desa-mentari/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616141435",layout:"default",error:a,serverRendered:true,routePath:"\u002Fwork\u002Fpoor-economics-of-desa-mentari",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c8c0ac87"}}}}(null));
